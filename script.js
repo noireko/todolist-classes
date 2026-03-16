4b8ffbf355596c7ec97307364951fe9f015f4556
noireko <162654411+noireko@users.noreply.github.com>
2026-03-16 14:49:10 -0300

    Implement ToDoList class for managing todos
    Added ToDoList class to manage todo items with methods to add, list, and mark todos as complete.

--- script.js
+++ script.js
@@ -16,5 +16,72 @@ console.log("${this.description}" marked as complete!);
 
 }
 
+class ToDoList {
+
+constructor() {
+
+this.todos = []; 
+}
+
+addTodo(description) {
+
+const newTodo = new ToDo(description); 
+
+this.todos.push(newTodo);
+
+this.renderTodoList(); 
 
 }
+
+listTodos() {
+
+return this.todos;
+
+}
+
+markTodoComplete(index) {
+
+if (index >= 0 && index < this.todos.length) {
+
+this.todos[index].markComplete();
+
+this.renderTodoList();
+
+}
+
+}
+
+renderTodoList() {
+
+const todoListElement = document.getElementById('todoList');
+
+todoListElement.innerHTML = '';
+
+this.todos.forEach((todo, index) => {
+
+const listItem = document.createElement('li');
+
+listItem.textContent = todo.description;
+
+if (todo.completed) {
+
+listItem.classList.add('completed');
+
+}
+
+
+const completeButton = document.createElement('button');
+
+completeButton.textContent = 'Complete';
+
+completeButton.onclick = () => this.markTodoComplete(index);
+
+listItem.appendChild(completeButton);
+
+todoListElement.appendChild(listItem);
+
+});
+
+}
+
+
